aaa60773814863156fca02bf4a0f239f65d41f50
Jv Shah <88554440+Jvshah124@users.noreply.git>
2023-09-16 12:07:28 +0530

    seo

---
+++ index.js
@@ -0,0 +1,47 @@
+"use strict";
+// -----------Element Toggle function -------------
+
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+// ------------Navbar variables------------------------
+
+const navbar = document.querySelector("[data-navbar]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+  });
+}
+
+// -------------Header scroll (sticky)---------------
+
+const header = document.querySelector("[data-header]");
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 10) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+// ----------Go Top--------------
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 800) {
+    goTopBtn.classList.add("active");
+  } else {
+    goTopBtn.classList.remove("active");
+  }
+});
